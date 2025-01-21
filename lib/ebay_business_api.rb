823af8c60ec1b21e96d4755f6e57498bcfe5edfc
Jean-Michel Keuleyan <jm.keuleyan@gmail.com>
2025-01-21 10:23:16 +0400

    Initial commit

---
+++ lib/ebay_business_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ebay_business_api/version"
+
+module EbayBusinessApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ebay_business_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EbayBusinessApi
+  VERSION = "0.1.0"
+end
