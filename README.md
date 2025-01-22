6076836c1c2a26e6782d8f6ec5abc279858a28af
Jean-Michel Keuleyan <jm.keuleyan@gmail.com>
2025-01-22 17:03:54 +0400

    Rework client and add user access token oauth logic

--- README.md
+++ README.md
@@ -2,4 +2,69 @@
 
 A gem to interact with eBay API using LedgerSync operations.
 
-## WIP
+## Setup eBay Developer Interface
+
+- Before using this gem, you'll need to set up a Developer account on https://developer.ebay.com/.
+- Make sure to create your first application and obtain your `CLIENT ID` and `CLIENT SECRET` keys from (https://developer.ebay.com/my/keys)
+- Additionally, register a test user (`Register a new Sandbox user`) and obtain a token for that user from (https://developer.ebay.com/my/auth)
+
+At this point, you should have your client keys ( `CLIENT ID`/`CLIENT SECRET`) and your user keys (`REDIRECT_URI`).
+
+## Client initialization
+
+To initialize the client, you'll need to set environment variables. Some of these variables are predefined (such as endpoints and environments), while others (like credentials) must be set manually:
+```sh
+CLIENT_ID=
+CLIENT_SECRET=
+REDIRECT_URI=
+
+CLIENT_ID_SANDBOX=
+CLIENT_SECRET_SANDBOX=
+REDIRECT_URI_SANDBOX=
+```
+
+Once these credentials are set, you can initialize the client like this:
+```ruby
+client = EbayBusinessApi::Client.new
+```
+
+If you want to enable sandbox mode, pass a parameter:
+```ruby
+client = EbayBusinessApi::Client.new(sandbox: true)
+```
+
+If any of these credentials are missing, an error will be raised.
+
+## The OAuth process
+
+The eBay API uses an OAuth 2.0 authentication process to allow applications to securely interact with eBay services. There are two types of tokens that you can obtain depending on your use case:
+
+### 1. Application Access Token (Client Credentials Flow)
+
+- This flow is straightforward and requires only one step.
+- The application access token is used to interact with eBay’s public APIs.
+- It is valid for 2 hours and allows actions like pulling public data (e.g., product details, pricing, etc.).
+- Limitations: It cannot be used for actions that require user permissions, such as buying or selling items.
+
+How to get a token:
+```ruby
+client.application_access_token
+```
+
+### 2. User Access Token (Authorization Code Grant Flow)
+
+This is a two-step process and is required for actions that involve user-related permissions, such as buying, selling, or managing eBay accounts.
+
+- Step 1: Generate an OAuth URL with the required CLIENT_ID, REDIRECT_URI, and requested SCOPES. The user must visit this URL and log in to eBay to grant your app permissions.
+- Step 2: After the user authorizes your app, eBay redirects them back to the specified REDIRECT_URI with an authorization code. Use this code to request a user access token from the OAuth endpoint.
+
+Once obtained, the user access token can be used to perform user-specific actions like creating listings, managing orders, or accessing account data.
+
+How to get a token:
+```ruby
+# First step, generate the grant url
+client.build_consent_uri
+
+# Second step, once you received the authorization_code on your callback controller action
+client.user_access_token(authorization_code)
+```
--- lib/ebay_business_api/client.rb
+++ lib/ebay_business_api/client.rb
@@ -1,21 +1,109 @@
 # frozen_string_literal: true
 
 require 'typhoeus'
-require 'ledger_sync'
 require 'base64'
 require 'json'
+require 'uri'
 
 module EbayBusinessApi
-  class Client
-    attr_reader :client_id, :client_secret, :sandbox
+  class EbayBusinessApiError < StandardError; end
 
-    def initialize(client_id:, client_secret:, sandbox: false)
-      @client_id = client_id
-      @client_secret = client_secret
+  class Client # rubocop:disable Metrics/ClassLength
+    attr_reader :client_id, :client_secret, :redirect_uri, :sandbox
+
+    BASE_SCOPES = [
+      'https://api.ebay.com/oauth/api_scope',
+      'https://api.ebay.com/oauth/api_scope/buy.order.readonly',
+      'https://api.ebay.com/oauth/api_scope/buy.guest.order',
+      'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.marketing',
+      'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.inventory',
+      'https://api.ebay.com/oauth/api_scope/sell.account.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.account',
+      'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.fulfillment',
+      'https://api.ebay.com/oauth/api_scope/sell.analytics.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly',
+      'https://api.ebay.com/oauth/api_scope/buy.shopping.cart',
+      'https://api.ebay.com/oauth/api_scope/buy.offer.auction',
+      'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.finances',
+      'https://api.ebay.com/oauth/api_scope/sell.payment.dispute',
+      'https://api.ebay.com/oauth/api_scope/sell.item.draft',
+      'https://api.ebay.com/oauth/api_scope/sell.item',
+      'https://api.ebay.com/oauth/api_scope/sell.reputation',
+      'https://api.ebay.com/oauth/api_scope/sell.reputation.readonly',
+      'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription',
+      'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly',
+      'https://api.ebay.com/oauth/api_scope/sell.stores',
+      'https://api.ebay.com/oauth/api_scope/sell.stores.readonly'
+    ].join(' ').freeze
+
+    def initialize(sandbox: false)
+      @client_id = sandbox ? ENV['CLIENT_ID_SANDBOX'] : ENV['CLIENT_ID']
+      @client_secret = sandbox ? ENV['CLIENT_SECRET_SANDBOX'] : ENV['CLIENT_SECRET']
+      @redirect_uri = sandbox ? ENV['REDIRECT_URI_SANDBOX'] : ENV['REDIRECT_URI']
       @sandbox = sandbox
+
+      validate_credentials!
     end
 
-    def access_token
+    # Application Token Flow
+    # This is a one-step flow. It generates a 2 hours valid token.
+    # With this token, we can interact with eBay's public APIs.
+    # We can pull products, prices and other public data.
+    # It doesn't give rights to do some user-related actions (Buying, Selling...)
+    # https://developer.ebay.com/api-docs/static/oauth-client-credentials-grant.html
+    def application_access_token
+      @application_access_token ||= post_request_oauth_endpoint(headers, access_token_body)
+    end
+
+    # Credentials Grant Flow
+    # This is a two-step flow. It generates an url first and then a token.
+    # With the final token, you can do all user-related actions (Buying, Selling...)
+    # https://developer.ebay.com/api-docs/static/oauth-authorization-code-grant.html
+
+    # Step 1: Generate oAuth URL
+    # This method returns an URL. Once the eBay user navigates to this URL..
+    # .. he authorizes our eBay app to use his account to do some API actions.
+    def build_consent_uri
+      URI::HTTPS.build(
+        host: consent_endpoint_url,
+        path: '/oauth2/authorize',
+        query: URI.encode_www_form(
+          client_id: client_id, locale: 'en-US',
+          redirect_uri: redirect_uri, response_type: 'code',
+          scope: BASE_SCOPES
+        )
+      ).to_s
+    end
+
+    # Step 2: Retrieve user access token with authorization code from Step 1
+    def user_access_token(authorization_code)
+      @user_access_token ||= post_request_oauth_endpoint(
+        headers,
+        authorization_code_body(authorization_code)
+      )
+    end
+
+    private
+
+    def validate_credentials!
+      if client_id.nil? || client_secret.nil? || redirect_uri.nil?
+        raise '[eBay Business API] Set-up eBay credentials before using this gem !'
+      end
+
+      true
+    end
+
+    def post_request_oauth_endpoint(headers, body)
       response = Typhoeus.post(
         oauth_endpoint_url,
         { method: :post, headers: headers, body: body }
@@ -25,15 +113,15 @@ module EbayBusinessApi
 
       raise_error(response, data) unless data[:access_token]
 
-      data
+      data[:access_token]
     end
 
-    private
-
     def oauth_endpoint_url
-      return ENV['EBAY_PROD_ENDPOINT'] unless sandbox
+      sandbox ? ENV['OAUTH_EBAY_SANDBOX_ENDPOINT'] : ENV['OAUTH_EBAY_PROD_ENDPOINT']
+    end
 
-      ENV['EBAY_SANDBOX_ENDPOINT']
+    def consent_endpoint_url
+      sandbox ? ENV['CONSENT_EBAY_SANDBOX_URL'] : ENV['CONSENT_EBAY_PROD_URL']
     end
 
     def headers
@@ -47,26 +135,31 @@ module EbayBusinessApi
       Base64.strict_encode64("#{client_id}:#{client_secret}")
     end
 
-    def body
+    def access_token_body
       {
         'grant_type' => 'client_credentials',
         'scope' => 'https://api.ebay.com/oauth/api_scope'
       }
     end
 
+    def authorization_code_body(authorization_code)
+      {
+        'grant_type' => 'authorization_code',
+        'code' => authorization_code,
+        'redirect_uri' => redirect_uri
+      }
+    end
+
     def parse_response(response)
       JSON.parse(response.body, symbolize_names: true)
     rescue JSON::ParserError
-      raise StandardError, "Invalid JSON response: #{response.body}"
+      raise EbayBusinessApiError, "Invalid JSON response: #{response.body}"
     end
 
     def raise_error(response, data)
-      error_details = {
-        code: response.code,
-        response_headers: response.headers,
-        response_body: data
-      }
-      raise StandardError, error_details.to_s
+      error_details = "CODE: #{response.code} - #{data[:error]}: #{data[:error_description]}"
+
+      raise EbayBusinessApiError, error_details.to_s
     end
   end
 end
